64d35915eeb35e166b104cd2e1b6e896242cb70b
Rishi <rishi.srighakollapu@gmail.com>
2026-04-29 23:07:32 +0530

    Update db.js with TiDB SSL settings

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Rishi@2006',
-    database: 'exam_system',
+    // Render lo manam set chese Environment Variables nundi details teesukuntundi
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
-    connectionLimit: 10
-}).promise(); // Promise valla code inka clean ga untundi
+    connectionLimit: 10,
+    // Cloud Databases (like Aiven/TiDB) ki SSL chala important
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).promise();
 
 module.exports = db;
